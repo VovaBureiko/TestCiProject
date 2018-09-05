37f5558b14711593bfd185b8e9db06d26b1ffb50
VovaBureiko <vovabureiko95@gmail.com>
2018-09-05 23:08:58 +0300

    Added tests to project

---
+++ TestCIProject.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestCIProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace TestCIProject.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        private readonly HomeController _controller;
+
+        public HomeControllerTest()
+        {
+            _controller = new HomeController();
+        }
+
+        [Fact]
+        public void String_Index_ViebagWithString()
+        {
+            // Arrange
+            var word = "anotherWord";
+
+            // Act
+            _controller.Index(word);
+
+            // Assert
+
+            Assert.Equal(word, _controller.ViewBag.word);
+        }
+    }
+}
