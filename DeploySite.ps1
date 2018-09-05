db92323215794eb249c41eb194f99bc623b906d5
VovaBureiko <vovabureiko95@gmail.com>
2018-09-05 18:40:28 +0300

    Change publish script

--- DeploySite.ps1
+++ DeploySite.ps1
@@ -4,26 +4,9 @@ $password = convertto-securestring -AsPlainText -Force -String "1234QazWsxEdc";
 $credential = new-object -typename System.Management.Automation.PSCredential -argumentlist "upingskills",$password;
 $session = New-PSSession -ComputerName 40.87.135.119 -port 5985 -Credential @credential;
 $websitePath = 'C:\TestCISite\';
-$artefactPath = 'C:\TeamCity\buildAgent\work\e55ca005f8c3e807\publish\*';
+$artefactPath = 'C:\TeamCity\buildAgent\work\cfb7413927a8802c\publish\*';
 $webSitePool = 'DefaultAppPool';
 $websiteName = 'TestCISyte';
-
-
-try {
-    if (Invoke-Command -ScriptBlock { Test-Path -Path $using:websitePath} -Session $session) {
-        Write-Information -Message "Folder is enable";
-        DeploySite;
-    }
-    else {
-        Write-Information -Message "Folder is not enabled";
-        Invoke-Command -ScriptBlock {New-Item -ItemType Directory -Path ${$using:websitePath;} } -Session $session;
-        DeploySite;
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.Message;
-    Write-Error -Message $ErrorMessage;
-}
 
 function DeploySite {
     Write-Information -Message "Start deploy files";
@@ -41,4 +24,21 @@ function DeploySite {
         Start-WebAppPool -Name $using:webSitePool;
     } -Session $session;
     Write-Information -Message "Website ${$using:websiteName} is working now";
+}
+
+
+try {
+    if (Invoke-Command -ScriptBlock { Test-Path -Path $using:websitePath} -Session $session) {
+        Write-Information -Message "Folder is enable";
+        DeploySite;
+    }
+    else {
+        Write-Information -Message "Folder is not enabled";
+        Invoke-Command -ScriptBlock {New-Item -ItemType Directory -Path ${$using:websitePath;} } -Session $session;
+        DeploySite;
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.Message;
+    Write-Error -Message $ErrorMessage;
 }
